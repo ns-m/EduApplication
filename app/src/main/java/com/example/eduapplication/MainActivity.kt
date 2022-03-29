adc5191ddb692ba8452aec33c66abf41883fdfc8
KMV <9117479@gmail.com>
2022-03-29 23:29:36 +0300

    AndSt 4.2

--- app/src/main/java/com/example/eduapplication/MainActivity.kt
+++ app/src/main/java/com/example/eduapplication/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.eduapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("MylogMAct", "onCreate")
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.d("MylogMAct", "onStart")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d("MylogMAct", "onResume")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MylogMAct", "onDestroy")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d("MylogMAct", "onStop")
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("MylogMAct", "onRestart")
     }
 }
