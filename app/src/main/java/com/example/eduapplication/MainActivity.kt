1e7b0eb179b0aa45e057db79b08135146dd44b7e
KMV <9117479@gmail.com>
2022-04-05 17:56:30 +0300

    Neco Edu app 9 and 10

--- app/src/main/java/com/example/eduapplication/MainActivity.kt
+++ app/src/main/java/com/example/eduapplication/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.eduapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import com.example.eduapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+/*    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("MylogMAct", "onCreate")
@@ -29,5 +31,28 @@ class MainActivity : AppCompatActivity() {
     override fun onRestart() {
         super.onRestart()
         Log.d("MylogMAct", "onRestart")
+    }*/
+    lateinit var bindingClass: ActivityMainBinding
+    var numberX:Int = 324
+    var numberY:Int = 24
+    override fun onCreate(myArg: Bundle?){
+        super.onCreate(myArg)
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+/*        bindingClass.magicButton.setOnClickListener{
+            bindingClass.magicTextView.visibility = View.GONE
+        }*/
+        bindingClass.addButton.setOnClickListener{
+            val result = numberX + numberY
+            bindingClass.numbersResult.text = "Result ADD = $result"
+        }
+        bindingClass.subtractButton.setOnClickListener{
+            val result = numberX - numberY
+            bindingClass.numbersResult.text = "Result Subtract = $result"
+        }
+        bindingClass.multiplyButton.setOnClickListener{
+            val result = numberX * numberY
+            bindingClass.numbersResult.text = "Result Multiply = $result"
+        }
     }
 }
