60abfa2f2cbde4ef7f15e24bfc5c1b3008cc88e4
KMV <9117479@gmail.com>
2022-04-07 17:53:59 +0300

    NC Lesson16b

--- app/src/main/java/com/example/eduapplication/TestActivity2.kt
+++ app/src/main/java/com/example/eduapplication/TestActivity2.kt
@@ -1,11 +1,22 @@
 package com.example.eduapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class TestActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test2)
+    }
+
+    fun onClickGoMain(view: View){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+    fun onClickGoSecond(view: View){
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
     }
 }
