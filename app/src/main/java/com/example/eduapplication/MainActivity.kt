ac6d83ff1d1bef4d813dde3bee260c6d71c39d7f
KMV <9117479@gmail.com>
2022-04-12 22:37:14 +0300

    NC Lesson22task

--- app/src/main/java/com/example/eduapplication/MainActivity.kt
+++ app/src/main/java/com/example/eduapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 
+/*
 class MainActivity : AppCompatActivity() {
     val investArray = arrayOf(120000, 80000, 50000, 45000, 23000, 71000)
     val profitArray = arrayOf(142000, 94000, 62000, 51000, 24000, 77000)
@@ -17,5 +18,33 @@ class MainActivity : AppCompatActivity() {
             Log.d("MyActLog", "Output Data -/- ${resultArray[index]}")
         }
 
+    }
+}*/
+class MainActivity : AppCompatActivity() {
+    val bad = 0..3
+    val normal = 4..6
+    val nice = 7..9
+    val excellent = 10
+    val gradeArray = arrayOf(4, 7, 3, 6, 10, 2)
+    val nameArray = arrayOf("Антон", "Егор", "Маша", "Светлана", "Юля", "Семен")
+    val badArray = ArrayList<String>()
+    val normalArray = ArrayList<String>()
+    val niceArray = ArrayList<String>()
+    val excellentArray = ArrayList<String>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        for ((index, name) in nameArray.withIndex()){
+            when(gradeArray[index]){
+                in bad -> badArray.add("Bad grade: Student $name - ${gradeArray[index]}")
+                in normal -> normalArray.add("Normal grade: Student $name - ${gradeArray[index]}")
+                in nice -> niceArray.add("Nice grade: Student $name - ${gradeArray[index]}")
+                excellent -> excellentArray.add("Excellent grade: Student $name - ${gradeArray[index]}")
+            }
+        }
+        badArray.forEach { Log.d("MyActLog", it) }
+        normalArray.forEach { Log.d("MyActLog", it) }
+        niceArray.forEach { Log.d("MyActLog", it) }
+        excellentArray.forEach { Log.d("MyActLog", it) }
     }
 }
